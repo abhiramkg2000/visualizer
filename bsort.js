3974e02e8ad6af56f236f1c16d9d3a2167a4eec4
abhiramkg <48902030+abhiramkg2000@users.noreply.github.com>
2022-04-17 14:24:04 +0530

    Update bsort.js

--- bsort.js
+++ bsort.js
@@ -1,86 +1,90 @@
-var array=document.getElementById("array-elements");
-var btn1=document.getElementById("btn");
-var btn2=document.getElementById("clr");
-var btn3=document.getElementById("start");
-var container=document.getElementById("contain");
-let squares=[];
-let timeout=1000;
-btn1.onclick=function display(){
-    var j=0;
-    var i=0;
-    swap=0;
-    var temp=array.value;
-    const result=temp.split(" ");
-    for(i=0;i<result.length;i++)
-    {
-        var array_item=document.createElement("div");
-        array_item.setAttribute("id",i+1);
-        array_item.innerHTML=result[i];
-        array_item.classList.add("box");
-        /*array_item.classList.add("paragraph-head");
-        array_item.appendChild(document.createTextNode((i+1)+" Pass Value: "));*/
-        container.append(array_item);
-        squares.push(array_item);
-    }
+var array = document.getElementById("array-elements");
+var btn1 = document.getElementById("insert");
+var btn2 = document.getElementById("start");
+var btn3 = document.getElementById("clr");
+var container = document.getElementById("contain");
+var status_heading=document.getElementById("status_heading");
+let squares = [];
+
+btn1.onclick = function display() {
+  var j = 0;
+  var i = 0;
+  var k=0;
+  swap = 0;
+  var temp = array.value;
+  const result = temp.split(" ");
+  for (i = 0; i < result.length; i++) {
+    var array_item = document.createElement("p");
+    array_item.setAttribute("id", i + 1);
+    array_item.innerHTML = result[i];
+    array_item.classList.add("box");
+    squares.push(array_item);
+    container.append(array_item);
+  }
+  status_heading.innerHTML="";
+};
+
+btn2.onclick = function bubble_sort() {
+  status_heading.innerHTML="In Progress";
+  Tutor();
+};
+
+btn3.onclick = function reset() {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+  while (squares.length) {
+    squares.pop();
+  }
+  status_heading.innerHTML="";
+  array.value="";
+};
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
-btn3.onclick=function bubble_sort(){
-    setTimeout(()=>{
-        for(i=0;i<squares.length-1;i++)
+
+async function Tutor() {
+  
+  for (i = 0; i < (squares.length - 1); i++) 
+  {
+        for (j = 0; j < (squares.length - i-1); j++) 
         {
-            for(j=0;j<squares.length-i-1;j++)
-            {   
-            //setTimeout(()=>{
-                if(parseInt(squares[j].innerHTML)>parseInt(squares[j+1].innerHTML))
+            console.log("i= ",i," j= ",j);
+            squares[j].classList.add("active");
+            squares[j + 1].classList.add("active");
+            console.log("squares ",container);
+            if (parseInt(squares[j].innerHTML) > parseInt(squares[j + 1].innerHTML)) 
+            {
+                squares[j+1].classList.add("move-left");
+                squares[j].classList.add("move-right");
+                swap = squares[j];
+                squares[j]= squares[j + 1];
+                squares[j + 1] = swap;
+                console.log("comparing "+squares[j].innerHTML+" and "+squares[j+1].innerHTML);
+                await sleep(2000);
+                while (container.firstChild) 
                 {
-                    if(j>0){
-                        squares[j-1].classList.remove("active")
-                        squares[j].classList.remove("active")
-                    }
-                    /*squares[j].classList.add("active")
-                    squares[j+1].classList.add("active")*/
-                    squares[j].classList.add("active")
-                    squares[j+1].classList.add("active")
-                    //setTimeout(()=>{
-                        swap=squares[j].innerHTML;
-                        squares[j].innerHTML=squares[j+1].innerHTML;
-                        squares[j+1].innerHTML=swap;
-                    //},1000)
-                    /*setTimeout(()=>{
-                    squares[j].classList.remove("active")
-                    squares[j+1].classList.remove("active")
-                    },1000)*/
-                    /*if(j===squares.length){
-                        setTimeout(()=>{
-                            squares[j].classList.remove("active")
-                            squares[j+1].classList.remove("active")
-                        },2000)
-                    }*/
-                    
+                  container.removeChild(container.firstChild);
                 }
-                
+                for (let k = 0; k < squares.length; k++) 
+                {
+                  var array_item = document.createElement("p");
+                  array_item.setAttribute("id", k + 1);
+                  array_item.innerHTML = squares[k].innerHTML;
+                  console.log(" "+squares[k].innerHTML+" ");
+                  array_item.classList.add("box");
+                  squares[k].classList.remove("move-left");
+                  squares[k].classList.remove("move-right");
+                  squares[k].classList.remove("active");
+                  container.append(squares[k]);
+                }
             }
-            if(j>=0){
-                setTimeout(()=>{
-                    squares[j].classList.remove("active")
-                    squares[j+1].classList.remove("active")
-                },1000)
-            }
+            await sleep(2000);
+            squares[j].classList.remove("active");
+            squares[j+1].classList.remove("active");
+            await sleep(2000);
         }
-        array.value="";
-    },2000)
-    timeout+=2000;
-}
-btn2.onclick=function reset(){
-    while(container.firstChild)
-    {
-        container.removeChild(container.firstChild);
     }
-    while(squares.length){
-        squares.pop();
-    }
-    
+    status_heading.innerHTML="Completed";
 }
-/*var array_item=document.createElement("p");
-array_item.classList.add("paragraph");
-array_item.appendChild(document.createTextNode("i= "+i+" j= "+j+" array is: "+result));
-container.appendChild(array_item);*/
